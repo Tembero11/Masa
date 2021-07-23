4598a0b5a38ac368ff7a07813e91a946f2ce89e6
Tembero <silver.teemu@gmail.com>
2021-07-23 12:30:19 +0300

    Initial Commit

---
+++ index.ts
@@ -0,0 +1,46 @@
+import Discord from "discord.js";
+import fs from "fs";
+import path from "path";
+import { start, stop, restart, getSeed } from "./serverHandler";
+
+export const config = JSON.parse(fs.readFileSync(path.join(__dirname, "config.json"), {encoding: "utf-8"}));
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  if (client.user) {
+    console.log(`Logged in as ${client.user.tag}!`);
+  }
+});
+
+client.on("message", msg => {
+  if (msg.content.startsWith(config["prefix"])) {
+    switch (msg.content.substr(config["prefix"].length)) {
+      case "start":
+        msg.reply("Server starting now... :slight_smile:");
+
+        start();
+        break;
+      
+      case "stop":
+        msg.reply("Stopping server... :cry:");
+  
+        stop();
+        break;
+      
+      case "restart":
+        msg.reply("Retarding Server...");
+  
+        restart();
+        break;
+
+      case "easteregg":
+        msg.reply(config["easteregg"][Math.floor(Math.random() * config["easteregg"].length)]);
+        break;
+      default:
+        break;
+    }
+  }
+});
+
+client.login(config["token"]);
---
+++ serverHandler.ts
@@ -0,0 +1,47 @@
+import { spawn, ChildProcessWithoutNullStreams } from "child_process";
+import { config } from "./index";
+
+let commandProcess: ChildProcessWithoutNullStreams | undefined;
+
+
+let stdoutLastLine: string | undefined;
+
+
+export const start = () => {
+  if (!commandProcess) {
+    commandProcess = spawn(config["command"], {shell: true});
+
+
+    commandProcess.stdout.on("data", (data) => {
+      stdoutLastLine = data.toString();
+      console.log(`[${commandProcess?.pid || ""}]${stdoutLastLine}`);
+    });
+
+    commandProcess.stdout.on("close", () => {
+      stdoutLastLine = undefined;
+      console.log("Process stopped!");
+    });
+  }
+}
+
+export const stop = () => {
+  if (commandProcess) {
+    commandProcess.stdin.write("stop\n");
+  }
+}
+
+export const restart = () => {
+  stop();
+  start();
+}
+
+export const getSeed = (): string => {
+  if (commandProcess) {
+    commandProcess.stdin.write("seed\n");
+    if (stdoutLastLine) {
+      return stdoutLastLine;
+    }
+  }
+
+  return "";
+}
