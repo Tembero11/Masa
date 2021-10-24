028cd0ec7fc8c2cc0564b602d0fe73077dde6740
Tembero <silver.teemu@gmail.com>
2021-10-24 14:17:34 +0300

    changed config loader

--- index.ts
+++ index.ts
@@ -3,13 +3,13 @@ import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
 import fs from "fs";
 import path from "path";
-import { isAllowedChannel, setPresence, usesPrefix, } from "./src/helpers";
+import { isAllowedChannel, setPresence } from "./src/helpers";
 import { Presence } from "./src/serverHandler";
 import setup from "./src/setup";
-import { CONFIG_TYPE, createConfig, loadConfig } from "./src/config";
+import { CONFIG_TYPE, createConfig, DefaultConfig, loadConfig } from "./src/config";
 import commands from "./src/commands/commands";
 
-export let config: { [key: string]: any };
+export let config: DefaultConfig;
 
 export const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS] });
 
@@ -68,7 +68,7 @@ try {
       console.log("Please edit the config/config.yml file in the bot directory.");
       process.exit();
     } else {
-      config = loadConfig(CONFIG_TYPE.General.path);
+      config = loadConfig("General");
 
 
       console.log("Config was successfully loaded!");
@@ -76,81 +76,24 @@ try {
       client.login(config["token"]);
     }
   });
-
-
-  // if (fs.existsSync(configPath)) {
-  //   config = yaml.load(fs.readFileSync(configPath, {encoding: "utf-8"})) as Object;
-
-  //   console.log("Config was successfully loaded!");
-
-  // }else {
-  //   inquirer.prompt([
-  //     {
-  //       type: "confirm",
-  //       message: '"config.yml" not found. Do you want to run a setup?',
-  //       name: "runSetup",
-  //     }
-  //   ]).then((result) => {
-  //     if (result.runSetup) {
-  //       inquirer.prompt([
-  //         {
-  //           type: "input",
-  //           message: "What's the server called?",
-  //           name: "serverName",
-  //         },
-  //         {
-  //           type: "input",
-  //           message: "Enter the bot token here",
-  //           name: "token",
-  //         },
-  //         {
-  //           type: "confirm",
-  //           message: "Do you want the bot to be able to show players that are online?",
-  //           name: "showPlayers",
-  //         },
-  //         {
-  //           type: "confirm",
-  //           message: "Do you want to make the seed public?",
-  //           name: "showWorldSeed",
-  //         },
-  //         {
-  //           type: "confirm",
-  //           message: "Do you want to enable backups?",
-  //           name: "useBackups",
-  //         },
-  //       ]).then((result) => {
-  //       }).catch((err) => console.log("An unknown error occured. Setup failed!"));
-  //     }else {
-  //       fs.writeFileSync(configPath, defaultConfig, {encoding: "utf-8"});
-  //       console.log("Please edit the config.yml file in the bot directory.");
-  //       process.exit();
-  //     }
-  //   }).catch((err) => {
-  //     if (err) {
-  //       fs.writeFileSync(configPath, defaultConfig, {encoding: "utf-8"});
-  //       console.log("Please edit the config.yml file in the bot directory.");
-  //       process.exit();
-  //     }
-  //   })
-  // }
 } catch (err) {
   throw err;
 }
 
 
-process.on("uncaughtException", (err) => {
-  const logs = path.join(__dirname, "logs");
+// process.on("uncaughtException", (err) => {
+//   const logs = path.join(__dirname, "logs");
 
-  if (!fs.existsSync(logs)) {
-    fs.mkdirSync(logs);
-  }
+//   if (!fs.existsSync(logs)) {
+//     fs.mkdirSync(logs);
+//   }
 
-  let date = new Date();
-  let logName = `${date.getDate()}.${date.getMonth() + 1}-${date.getHours()}.${date.getMinutes()}.log`;
+//   let date = new Date();
+//   let logName = `${date.getDate()}.${date.getMonth() + 1}-${date.getHours()}.${date.getMinutes()}.log`;
 
-  const errorMessage = `${err.name}:\n${err.message}\n${err.stack}`;
+//   const errorMessage = `${err.name}:\n${err.message}\n${err.stack}`;
 
-  fs.writeFileSync(path.join(logs, logName), errorMessage);
+//   fs.writeFileSync(path.join(logs, logName), errorMessage);
 
-  throw err;
-});
+//   throw err;
+// });
