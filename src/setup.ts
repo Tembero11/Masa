38e71e6e8c2f4fa41f67867b153059cb478fa8ab
Tembero <silver.teemu@gmail.com>
2021-10-24 13:57:47 +0300

    added serverInit call

--- src/setup.ts
+++ src/setup.ts
@@ -1,31 +1,32 @@
 import { BACKUP_TYPE, createBackupsFolder, createNewBackup } from "./backup";
 import { ConsoleColor, createDateTimeString, parseDateTimeString, serverDir } from "./helpers";
 import fs from "fs";
-import { config } from "..";
-import { isServerJoinable } from "./serverHandler";
-import { socketConnect } from "./socket/connect";
+import { config } from "../";
+import { serverInitializer } from "./serverHandler";
 
 const setup = async() => {
+    serverInitializer(config["servers"]);
+
     await createBackupsFolder(BACKUP_TYPE.AutomaticBackup);
     await createBackupsFolder(BACKUP_TYPE.UserBackup);
     await createServerFolder();
 
     let automaticBackups = config["backup"]["automaticBackups"];
 
-    if (automaticBackups >= 1) {
-        console.log('Automatic backups are enabled.');
-        setInterval(() => {
-            if (isServerJoinable) {
-                createNewBackup(BACKUP_TYPE.AutomaticBackup, true).then((backupName) => {
-                    console.log("An automatic backup was succesfully created! %s", backupName);
-                }).catch((err) => {
-                    console.warn(ConsoleColor.FgYellow, err, ConsoleColor.Reset);
-                });
-            }
-        }, automaticBackups * 1000 * 60);
-    }else {
-        console.log('Automatic backups are disabled, you can change this by changing the "automaticBackups" to a value greater than 1.');
-    }
+    // if (automaticBackups >= 1) {
+    //     console.log('Automatic backups are enabled.');
+    //     setInterval(() => {
+    //         if (isServerJoinable) {
+    //             createNewBackup(BACKUP_TYPE.AutomaticBackup, true).then((backupName) => {
+    //                 console.log("An automatic backup was succesfully created! %s", backupName);
+    //             }).catch((err) => {
+    //                 console.warn(ConsoleColor.FgYellow, err, ConsoleColor.Reset);
+    //             });
+    //         }
+    //     }, automaticBackups * 1000 * 60);
+    // }else {
+    //     console.log('Automatic backups are disabled, you can change this by changing the "automaticBackups" to a value greater than 1.');
+    // }
 
 
     return true;
